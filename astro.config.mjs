eefd822cf92143134c4b30f456fd250d70e5252f
Christoffer Rydeståhl <kristoffer@oholics.com>
2023-11-20 10:24:28 +0100

    more config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ const m2dxOptions = {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nebulix.unfolding.io",
+  site: "https://tranquil-fox-6cf545.netlify.app",
   integrations: [
     mdx({}),
     sitemap(),
